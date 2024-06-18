42031517082a75a66390c15d1aa44774823150dc
tiaede00 <workmail.edem@gmail.com>
2024-06-18 17:44:24 -0400

    - adds server base path

--- jupyter_vscode_web_proxy/__init__.py
+++ jupyter_vscode_web_proxy/__init__.py
@@ -32,6 +32,7 @@ def run_vscode_web() -> Dict[str, Any]:
         cmd = [
             _get_code_server_executable('code-server'),
             'serve-local' + extension_arg,
+            '--server-base-path=vscode-web'
             '--port=' + str(port),
             '--accept-server-license-terms',
             '--host=0.0.0.0',
